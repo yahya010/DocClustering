3d2dff28afb741f0ede78599abc36ceb1f444cc9
yahya010 <49173233+yahya010@users.noreply.github.com>
2022-11-28 17:45:33 -0500

    Add files via upload

---
+++ capstone.py
@@ -0,0 +1,25 @@
+from sklearn.datasets import fetch_20newsgroups
+newsgroups_train = fetch_20newsgroups(subset='train')
+from pprint import pprint
+pprint(list(newsgroups_train.target_names))
+['alt.atheism',
+ 'comp.graphics',
+ 'comp.os.ms-windows.misc',
+ 'comp.sys.ibm.pc.hardware',
+ 'comp.sys.mac.hardware',
+ 'comp.windows.x',
+ 'misc.forsale',
+ 'rec.autos',
+ 'rec.motorcycles',
+ 'rec.sport.baseball',
+ 'rec.sport.hockey',
+ 'sci.crypt',
+ 'sci.electronics',
+ 'sci.med',
+ 'sci.space',
+ 'soc.religion.christian',
+ 'talk.politics.guns',
+ 'talk.politics.mideast',
+ 'talk.politics.misc',
+ 'talk.religion.misc']
+print(newsgroups_train.filenames.shape)
